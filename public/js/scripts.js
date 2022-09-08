4135e6dba92eb05bf3439972fdbecb2cc5c0f9f1
Glitch (solstice-palm-bone) <none>
2022-09-08 12:32:22 -0400

    test todo list

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,191 @@
-// Add some Javascript code here, to run on the front end.
+let selectAll = document.querySelector("#select_all_toggler");
+let input = document.querySelector("#input");
+let todoList = document.querySelector("#todo_list");
+let showStates = document.querySelector(".state");
+let counter = document.querySelector(".counter");
+let clearBtn = document.querySelector("#delete");
+let classify = document.querySelector("#classify_toggler");
 
-console.log("Welcome to assignment 2!")
+classify.onclick = (e) => {
+  if (e.target.matches("input")) {
+    debugger;
+    console.log(e);
+    todoList.classList.remove("all", "incomplete", "complete");
+    todoList.classList.add(e.target.value);
+  }
+};
+
+selectAll.onclick = (e) => {
+  let complete_item = todoList.querySelectorAll("li.complete");
+  if (complete_item.length == todoList.children.length) {
+    Array.from(todoList.children).forEach((it) => {
+      it.classList.remove("complete");
+      it.firstElementChild.checked = false;
+    });
+  } else {
+    Array.from(todoList.children).forEach((it) => {
+      it.classList.add("complete");
+      it.firstElementChild.checked = true;
+    });
+  }
+  setActions();
+  refresh();
+};
+
+input.onkeyup = function (e) {
+  if (e.key === "Enter") {
+    add({
+      content: e.target.value.trim(),
+      complete: false,
+    });
+    e.target.value = "";
+    refresh();
+  }
+};
+
+clearBtn.onclick = (e) => {
+
+  let tobeDeleted = document.querySelectorAll(".complete");
+  tobeDeleted.forEach((it) => {
+    todoList.removeChild(it);
+  });
+
+  let item = document.getElementById("delete");
+  let json = {
+    id: "delete",
+  };
+  let body = JSON.stringify(json);
+  clearBtn.style.display = "none";
+
+  fetch("/delete", {
+    method: "POST",
+    body,
+  }).then(async function (response) {
+    if (response.status === 200) {
+      alert("clear successfully");
+    } else {
+      alert("issue found");
+    }
+    let responseInfo = await response.json();
+    refresh(responseInfo);
+    console.log(responseInfo);
+  });
+
+  return false;
+};
+
+function setActions() {
+  let incompletes = todoList.querySelectorAll(
+    "input:first-child:not(:checked)"
+  );
+  let complete_item = todoList.querySelectorAll("input:first-child:checked");
+  if (incompletes.length) {
+    selectAll.checked = false;
+  } else {
+    selectAll.checked = true;
+  }
+  counter.textContent = incompletes.length + " to finish";
+  if (complete_item.length) {
+    clearBtn.style.display = "inline-block";
+  } else {
+    clearBtn.style.display = "none";
+  }
+}
+
+let todos = JSON.parse(localStorage.todos) || [];
+
+todos.forEach((todo) => {
+  add(todo);
+});
+
+function add(todo) {
+  let todoText = todo.content;
+  if (todoText.trim() !== "") {
+    this.value = "";
+    let li = document.createElement("li");
+    li.classList.add("item_todo");
+    todo.complete && li.classList.add("complete");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.complete;
+
+    checkbox.onchange = (e) => {
+      if (checkbox.checked) {
+        li.classList.add("complete");
+      } else {
+        li.classList.remove("complete");
+      }
+      setActions();
+      refresh();
+    };
+
+    let span = document.createElement("span");
+    span.textContent = todoText;
+    span.ondblclick = (e) => {
+      li.classList.add("editing");
+      setTimeout(() => {
+        editBox.focus();
+      });
+    };
+
+    let editBox = document.createElement("input");
+    editBox.type = "text";
+    editBox.value = todoText;
+    editBox.onkeyup = (e) => {
+      if (e.key == "Enter") {
+        span.textContent = editBox.value;
+        li.classList.remove("editing");
+        refresh();
+      }
+    };
+    editBox.onblur = (e) => {
+      span.textContent = editBox.value;
+      li.classList.remove("editing");
+      refresh();
+    };
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.onclick = (e) => {
+      todoList.removeChild(li);
+      setActions();
+      if (todoList.children.length == 0) {
+        showStates.style.display = "none";
+      }
+      refresh();
+    };
+
+    li.append(checkbox, span, editBox, deleteBtn);
+    todoList.append(li);
+    setActions();
+  }
+
+  let status = document.querySelector(".state");
+  let content = document.querySelector("#input");
+  let json = {
+    complete: status.value,
+    content: content.value,
+  };
+  let body = JSON.stringify(json);
+  fetch("/submit", {
+    method: "POST",
+    body,
+  }).then(async function (response) {
+    let responseInfo = await response.json();
+    refresh(responseInfo);
+    console.log(responseInfo);
+  });
+
+  return false;
+}
+
+function refresh() {
+  let todos = Array.from(todoList.children).map((li) => {
+    return {
+      complete: li.firstElementChild.checked,
+      content: li.firstElementChild.nextElementSibling.textContent,
+    };
+  });
+  console.log(todos);
+  localStorage.todos = JSON.stringify(todos);
+}
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,101 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const appdata = [{ complete: "false", content: "Love my cat!" }];
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const http = require("http"),
+    fs = require("fs"),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library used in the following line of code
+    mime = require("mime"),
+    dir = "public",
+    port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  switch( request.url ) {
+    case '/':
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
+      break
+    case '/style.css':
+      sendFile( response, 'css/style.css' )
+      break
+    case '/script.js':
+      sendFile( response, 'js/script.js' )
+      break
+    default:
+      response.end( '404 Error: File Not Found' )
   }
-})
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-}
+});
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else {
+    sendFile(response, filename);
+  }
+};
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+const handlePost = function (request, response) {
+  let dataString = "";
 
-    // ... do something with the data here!!!
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+  if (request.url === "/submit") {
+    request.on("end", function () {
+      console.log(JSON.parse(dataString).DOB);
+      let data1 = JSON.parse(dataString);
+      // state = data1.complete,
+      // inputs = data1.content;
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+      console.log(data1);
+      // appdata.push(data1);
 
-   fs.readFile( filename, function( err, content ) {
+      response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+      response.write(JSON.stringify(appdata));
+      console.log(appdata);
+      response.end();
+    });
+  } else if (request.url === "/delete") {
+    request.on("end", function () {
+      let data2 = JSON.parse(dataString);
+      // state = data2.complete,
+      // inputs = data2.content;
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+      console.log(data2);
+      // appdata.remove(inputs);
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+      response.write(JSON.stringify(appdata));
+      console.log(appdata);
+      response.end();
+    });
+  }
+};
 
-     }else{
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
